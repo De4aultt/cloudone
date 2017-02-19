8dbb94d6a70719b5dc38f026f6bf7d25e450dbb8
Sasha Lavrenchuk <lavronsashka@gmail.com>
2017-02-19 15:12:16 +0200

    second commit

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,8 @@
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("clouds!");
-		System.out.println("lesson 1");
+		System.out.println("lesson1");
+                                System.out.println("lesson2");
 		}
 		
 		
