04b6f06697a55d87bb38880f8c51fcd2f7d779ff
you <you@example.com>
2017-02-19 14:57:21 +0200

    De4aultt's commit

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("clouds!");
 		System.out.println("lesson 1");
+		System.out.println("De4aultt change");
 		}
 		
 		
